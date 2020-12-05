63764868faa4037a530b581479f567ca8e164d67
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2020-12-05 11:44:28 +0200

    Start

---
+++ src/main/java/com/db/codelorianssocial/CodeloriansSocialApplication.java
@@ -0,0 +1,13 @@
+package com.db.codelorianssocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeloriansSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeloriansSocialApplication.class, args);
+    }
+
+}
