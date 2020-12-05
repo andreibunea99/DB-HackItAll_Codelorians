572b297eb6009587b901818c63cd511dfa09b635
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2020-12-05 12:53:18 +0200

    Command writer

--- src/main/java/com/db/codelorianssocial/controllers/HomeController.java
+++ src/main/java/com/db/codelorianssocial/controllers/HomeController.java
@@ -4,13 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Controller
 public class HomeController {
 
     boolean hidden = true;
 
+    public void writeInCommand() throws IOException {
+        String str = "Hello";
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/Discord/command.txt"));
+        writer.write(str);
+
+        writer.close();
+    }
+
     @GetMapping("/")
     public String mainPage(Model model) {
+        try {
+            writeInCommand();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         model.addAttribute("isHidden", hidden);
         return "index";
     }
@@ -19,9 +36,6 @@ public class HomeController {
     public String discordPage(Model model) {
         hidden = !hidden;
 
-        System.out.println(hidden);
-
-//        model.addAttribute("isHidden", hidden);
         return mainPage(model);
     }
 }
