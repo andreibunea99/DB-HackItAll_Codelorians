058c3fcf56960cedddae774b204aadd1f902390f
CosminBanica <56835268+CosminBanica@users.noreply.github.com>
2020-12-06 05:00:10 +0200

    stats

--- src/main/java/com/db/codelorianssocial/utils/Stats.java
+++ src/main/java/com/db/codelorianssocial/utils/Stats.java
@@ -2,12 +2,15 @@ package com.db.codelorianssocial.utils;
 
 import com.db.codelorianssocial.models.CurrentUser;
 
+import java.sql.Time;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Stats {
     public static void showStats(CurrentUser currUser) {
         for (Map.Entry<String, Long> entry : currUser.getTimeInRooms().entrySet()) {
-            System.out.println("Time spent in room " + entry.getKey() + " = " + entry.getValue());
+            long convert = TimeUnit.SECONDS.convert(entry.getValue(), TimeUnit.NANOSECONDS);
+            System.out.println("Time spent in room " + entry.getKey() + " = " + convert + "s.");
         }
     }
 }
